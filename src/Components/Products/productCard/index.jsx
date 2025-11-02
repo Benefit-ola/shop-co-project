2b7f4af1a7a0e1f0321de065f4c6c70ac8ce77b1
faidat <olawuyiodunolafaidat408.com>
2025-11-02 21:36:31 +0100

    added productCard component

--- src/Components/Products/productCard/index.jsx
+++ src/Components/Products/productCard/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const ProductDetails = () => {
+const ProductCard = () => {
   return (
     <div>
       
@@ -8,4 +8,4 @@ const ProductDetails = () => {
   )
 }
 
-export default  ProductDetails
+export default ProductCard
