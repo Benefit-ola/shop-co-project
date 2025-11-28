4c0e2d336cfc144bf607b177cd5b6f74284ee088
faidat <olawuyiodunolafaidat408.com>
2025-11-28 07:25:07 +0100

    working in progress

--- src/Components/Products/productDetailsSetion/index.jsx
+++ src/Components/Products/productDetailsSetion/index.jsx
@@ -1,5 +1,16 @@
 import React, { useState } from "react";
-import { circle, circle2, circle3, frame, frame10,  frame8, frame9, mg, mg2, mg3 } from "../../../constant";
+import {
+  circle,
+  circle2,
+  circle3,
+  frame,
+  frame10,
+  frame8,
+  frame9,
+  mg,
+  mg2,
+  mg3,
+} from "../../../constant";
 import { IoIosArrowForward } from "react-icons/io";
 import Button from "../../../../shared/Button";
 import { FaMinus, FaPlus } from "react-icons/fa6";
@@ -10,35 +21,35 @@ const ProductDetails = () => {
   const [activeimage, setActiveimage] = useState(productImage[0]);
   const [activeTab, setActiveTab] = useState(1);
   const products = [
-      {
-        id: 1,
-        image: frame,
-        text: "T-SHIRT WITH TAPE DETAILS",
-        stars: "⭐⭐⭐⭐⭐",
-        price: "$45.00",
-      },
-      {
-        id: 2,
-        image: frame8,
-        text: "SKINNY FIT JEANS",
-        stars: "⭐⭐⭐⭐⭐",
-        price: "$240",
-      },
-      {
-        id: 3,
-        image: frame9,
-        text: "SLEEVE STRIPED T-SHIRT",
-        stars: "⭐⭐⭐⭐⭐",
-        price: "$$180",
-      },
-      {
-        id: 4,
-        image: frame10,
-        text: "T-SHIRT WITH TAPE DETAILS",
-        stars: "⭐⭐⭐⭐⭐",
-        price: "$130",
-      },
-    ];
+    {
+      id: 1,
+      image: frame,
+      text: "T-SHIRT WITH TAPE DETAILS",
+      stars: "⭐⭐⭐⭐⭐",
+      price: "$45.00",
+    },
+    {
+      id: 2,
+      image: frame8,
+      text: "SKINNY FIT JEANS",
+      stars: "⭐⭐⭐⭐⭐",
+      price: "$240",
+    },
+    {
+      id: 3,
+      image: frame9,
+      text: "SLEEVE STRIPED T-SHIRT",
+      stars: "⭐⭐⭐⭐⭐",
+      price: "$$180",
+    },
+    {
+      id: 4,
+      image: frame10,
+      text: "T-SHIRT WITH TAPE DETAILS",
+      stars: "⭐⭐⭐⭐⭐",
+      price: "$130",
+    },
+  ];
 
   return (
     <div className="mx-[3rem] ">
@@ -98,9 +109,8 @@ const ProductDetails = () => {
           <h1 className="font-extrabold text-4xl mt-2">
             One Life Graphic T-shirt
           </h1>
-          
+
           <div className="flex gap-2 items-center">
-            
             <p className="mt-2">⭐⭐⭐⭐⭐</p>
             <p className="text-gray-300 ">4.5/5</p>
           </div>
@@ -140,9 +150,8 @@ const ProductDetails = () => {
                 X-Large
               </Button>
             </div>
-
           </div>
-           <hr />
+          <hr />
           <div className="flex gap-3 mt-4">
             <Button className="border flex gap-7 items-center  px-12 p-2 text-md rounded-full hover:bg-gray-300 text-black mt-3 ">
               <FaMinus />
@@ -166,24 +175,32 @@ const ProductDetails = () => {
           FAQs
         </p>
       </div>
-      <hr/>
+      <hr />
       {activeTab === 1 && <div className="">tab oneeeeeeeeeeeee</div>}
-      {activeTab === 2 && <div className="">
-        <RateReview/>
-        </div>}
-        <div>
-          <h2 className="text-center font-bold text-4xl mt-5">YOU MIGHT ALSO LIKE </h2>
-          <div className="grid grid-cols-4 gap-4 mt-10  text-center mb-10 ">
-{products.map((data,i)=>(
-  <div key={i} className=" ">
-                <img src={data.image} alt={data.text} className="mb-4 transition-transform duration-300 ease-in-out hover:scale-110 cursor-pointer" />
-                <h3 className="font-semibold mb-2">{data.text}</h3>
-                <p className="mb-2">{data.stars}</p>
-                <h4 className="font-bold">{data.price}</h4>
-              </div>
-))}
-          </div>
+      {activeTab === 2 && (
+        <div className="">
+          <RateReview />
         </div>
+      )}
+      <div>
+        <h2 className="text-center font-bold text-4xl mt-5">
+          YOU MIGHT ALSO LIKE{" "}
+        </h2>
+        <div className="grid grid-cols-4 gap-4 mt-10  text-center mb-10 ">
+          {products.map((data, i) => (
+            <div key={i} className=" ">
+              <img
+                src={data.image}
+                alt={data.text}
+                className="mb-4 transition-transform duration-300 ease-in-out hover:scale-110 cursor-pointer"
+              />
+              <h3 className="font-semibold mb-2">{data.text}</h3>
+              <p className="mb-2">{data.stars}</p>
+              <h4 className="font-bold">{data.price}</h4>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
--- src/pages/component/categoryPage/index.jsx
+++ src/pages/component/categoryPage/index.jsx
@@ -1,18 +1,91 @@
 import React from "react";
 import { FaRegCircle } from "react-icons/fa6";
-import { IoIosArrowForward, IoIosArrowUp } from "react-icons/io";
+import {
+  IoIosArrowDown,
+  IoIosArrowForward,
+  IoIosArrowUp,
+} from "react-icons/io";
 import { IoEllipse, IoEllipseOutline } from "react-icons/io5";
 import { TbListDetails } from "react-icons/tb";
 import Button from "../../../../shared/Button";
+import { frame10, frame2, frame3, frame4, frame5, frame6,  frame8, frame9 } from "../../../constant";
 // import { RiAlignVertically } from 'react-icons/ri'
 
 const CategoryPage = () => {
+  const products = [
+ 
+    {
+      id: 1,
+      image: frame8,
+      text: "SKINNY FIT JEANS",
+      stars: "⭐⭐⭐⭐⭐",
+      price: "$240",
+    },
+    {
+      id: 2,
+      image: frame9,
+      text: "SLEEVE STRIPED T-SHIRT",
+      stars: "⭐⭐⭐⭐⭐",
+      price: "$$180",
+    },
+    {
+      id: 3,
+      image: frame10,
+      text: "T-SHIRT WITH TAPE DETAILS",
+      stars: "⭐⭐⭐⭐⭐",
+      price: "$130",
+    },
+       {
+          id: 4,
+          image: frame2,
+          text: "SKINNY FIT JEANS",
+          stars: "⭐⭐⭐⭐⭐",
+          price: "$240",
+        },
+        {
+          id: 5,
+          image: frame3,
+          text: "SLEEVE STRIPED T-SHIRT",
+          stars: "⭐⭐⭐⭐⭐",
+          price: "$$180",
+        },
+        {
+          id: 6,
+          image: frame4,
+          text: "T-SHIRT WITH TAPE DETAILS",
+          stars: "⭐⭐⭐⭐⭐",
+          price: "$130",
+        },
+         {
+              id: 7,
+              image: frame5,
+              text: "VERTICAL STRIPED SHIRT",
+              stars: "⭐⭐⭐⭐⭐",
+              price: "$212",
+            },
+            {
+              id: 8,
+              image: frame6,
+              text: "COURAGE GRAPHIC T-SHIRT",
+              stars: "⭐⭐⭐⭐⭐",
+              price: "$145",
+            },
+                {
+                    id: 3,
+                    image: frame3,
+                    text: "SLEEVE STRIPED T-SHIRT",
+                    stars: "⭐⭐⭐⭐⭐",
+                    price: "$$180",
+                  },
+          
+  ];
+
   return (
     <>
       <div className="flex gap-4 mx-8 w-full my-8 ">
-        <div className="border shadow-md w-[20%] flex flex-col gap-4 rounded-md ">
+        <div className="border shadow-md p-[30px] flex flex-col gap-4 rounded-xl w-[300px] h-fit">
           <div>
-            <div className="flex items-center justify-between gap-10 ">
+            <div className="flex items-center justify-between gap-10  ">
               <h2>Filters</h2>
               <TbListDetails />
             </div>
@@ -20,31 +93,25 @@ const CategoryPage = () => {
           <hr />
           <div>
             <div className=" ">
-              <ul className="    text-[#5d5b5b] ">
+              <ul className="    text-[#5d5b5b]  ">
                 <div>
-                  {" "}
-                  <div className="flex items-center justify-between gap-10  space-y-4">
-                    {" "}
+                  <div className="flex items-center justify-between gap-10  space-y-6">
                     <li>T-shirts </li>
                     <IoIosArrowForward />
                   </div>
-                  <div className="flex items-center justify-between gap-10">
-                    {" "}
+                  <div className="flex items-center justify-between gap-10 space-y-6">
                     <li>Shorts</li>
                     <IoIosArrowForward />
-                  </div>{" "}
-                  <div className="flex items-center justify-between gap-10">
-                    {" "}
+                  </div>
+                  <div className="flex items-center justify-between gap-10 space-y-6">
                     <li>Shirts</li>
                     <IoIosArrowForward />
-                  </div>{" "}
-                  <div className="flex items-center justify-between gap-10">
-                    {" "}
+                  </div>
+                  <div className="flex items-center justify-between gap-10 space-y-6">
                     <li>Hoodie </li>
                     <IoIosArrowForward />
-                  </div>{" "}
-                  <div className="flex items-center justify-between gap-10">
-                    {" "}
+                  </div>
+                  <div className="flex items-center justify-between gap-10 space-y-6">
                     <li>Jeans </li>
                     <IoIosArrowForward />
                   </div>
@@ -54,9 +121,8 @@ const CategoryPage = () => {
             <div></div>
           </div>
           <div>
-            <div className="flex justify-between">
-              {" "}
-              <h2>color</h2>
+            <div className="flex justify-between  items-center mb-5">
+              <h2 className="font-bold text-lg">Colors</h2>
               <IoIosArrowUp />
             </div>
 
@@ -77,13 +143,12 @@ const CategoryPage = () => {
           </div>
           <hr />
           <div>
-            <div className="flex justify-between">
-              <h2>Size</h2>
+            <div className="flex justify-between items-center mb-5">
+              <h2 className="font-bold text-lg">Size</h2>
               <IoIosArrowUp />
             </div>
             <div className="grid grid-cols-2 gap-1">
-              
-              <Button className="border     text-sm rounded-full bg-[#ebe9e9]  text-black p-2 ">
+              <Button className="border     text-sm rounded-full bg-[#ebe9e9]  text-black p-2 space-y-6 ">
                 XX-Small
               </Button>
               <Button className="border    text-sm rounded-full bg-[#ebe9e9]  text-black  ">
@@ -114,44 +179,67 @@ const CategoryPage = () => {
           </div>
           <div>
             <hr />
-            <div className="flex justify-between">
-              {" "}
-              <h2>Dress Style</h2>
+            <div className="flex justify-between items-center mb-5 mt-5">
+              <h2 className="font-bold text-lg">Dress Style</h2>
               <IoIosArrowUp />
             </div>
-             <ul className="    text-[#5d5b5b] ">
-                <div>
-                  {" "}
-                  <div className="flex items-center justify-between gap-10  space-y-4">
-                    {" "}
-                    <li>T-shirts </li>
-                    <IoIosArrowForward />
-                  </div>
-                  <div className="flex items-center justify-between gap-10">
-                    {" "}
-                    <li>Shorts</li>
-                    <IoIosArrowForward />
-                  </div>{" "}
-                  <div className="flex items-center justify-between gap-10">
-                    {" "}
-                    <li>Shirts</li>
-                    <IoIosArrowForward />
-                  </div>{" "}
-                  <div className="flex items-center justify-between gap-10">
-                    {" "}
-                    <li>Hoodie </li>
-                    <IoIosArrowForward />
-                  </div>{" "}
-                  <div className="flex items-center justify-between gap-10">
-                    {" "}
-                    <li>Jeans </li>
-                    <IoIosArrowForward />
-                  </div>
+            <ul className="    text-[#5d5b5b] ">
+              <div>
+                <div className="flex items-center justify-between gap-10  space-y-4">
+                  <li>Casual </li>
+                  <IoIosArrowForward />
                 </div>
-              </ul>
+                <div className="flex items-center justify-between gap-10 space-y-6">
+                  <li>Formal</li>
+                  <IoIosArrowForward />
+                </div>
+                <div className="flex items-center justify-between gap-10 space-y-6">
+                  <li>Party</li>
+                  <IoIosArrowForward />
+                </div>
+                <div className="flex items-center justify-between gap-10   space-y-6">
+                  <li>Gym </li>
+                  <IoIosArrowForward />
+                </div>
+              </div>
+            </ul>
           </div>
+          <Button className="border bg-[black] text-white    p-2 text-md rounded-full hover:bg-gray-300  ">
+            Apply Filter
+          </Button>
         </div>
-        <div>shgd</div>
+        <div>
+          <div>
+            <h1 className="font-bold text-2xl">Casual</h1>
+            {/* <div> 
+    <p>Showing 1-10 of 100 Products</p>
+ 
+   
+  
+  </div> */}
+            {/* <div> 
+    <p>Sort by:</p>
+    <p className="cursor-pointer flex items-center text-center gap-1  px-3 py-2 w-[7rem]">
+                 Most Popular<IoIosArrowDown className="text-2xl" />
+              </p>
+ </div> */}
+          </div>
+
+           <div className="grid grid-cols-3 gap-4 mt-2  text-center   h-fit w-[90%] ">
+          {products.map((data, i) => (
+            <div key={i} className=" ">
+              <img
+                src={data.image}
+                alt={data.text}
+                className="mb-4 transition-transform duration-300 ease-in-out hover:scale-110 cursor-pointer"
+              />
+              <h3 className="font-semibold mb-2">{data.text}</h3>
+              <p className="mb-2">{data.stars}</p>
+              <h4 className="font-bold">{data.price}</h4>
+            </div>
+          ))}
+        </div>
+        </div>
       </div>
     </>
   );
